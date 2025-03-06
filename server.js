04fbc1daaec8ea34d853a69ecc9d92839b92a19f
mrbishalbaniya <mr.bishal.baniya@gmail.com>
2025-03-06 23:34:45 +0545

    first commit

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const io = new Server(server, {
 });
 
 let onlineUsers = {}; // Track online users: { socketId: username }
+let users = {}; // Store usernames with their socket IDs
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
@@ -19,6 +20,7 @@ io.on('connection', (socket) => {
     // Handle user joining
     socket.on('join', (username) => {
         onlineUsers[socket.id] = username;
+        users[username] = socket.id;
         console.log(`${username} joined the chat`);
         io.emit('online_users', Object.values(onlineUsers)); // Send updated list to all users
     });
@@ -33,11 +35,36 @@ io.on('connection', (socket) => {
         socket.to(data.to).emit('chat_message', { from: socket.id, message: data.message });
     });
 
+    // Handle user calling another user
+    socket.on('call_user', (data) => {
+        const targetSocketId = users[data.to]; // Get receiver's socket ID
+        if (targetSocketId) {
+            io.to(targetSocketId).emit('incoming_call', { from: data.from });
+        }
+    });
+
+    // Handle call acceptance
+    socket.on('accept_call', (data) => {
+        const callerSocketId = users[data.to];
+        if (callerSocketId) {
+            io.to(callerSocketId).emit('call_accepted', { from: data.from });
+        }
+    });
+
+    // Handle call rejection
+    socket.on('reject_call', (data) => {
+        const callerSocketId = users[data.to];
+        if (callerSocketId) {
+            io.to(callerSocketId).emit('call_rejected', { from: data.from });
+        }
+    });
+
     // Handle user disconnection
     socket.on('disconnect', () => {
         const username = onlineUsers[socket.id];
         if (username) {
             delete onlineUsers[socket.id];
+            delete users[username];
             console.log(`${username} left the chat`);
             io.emit('online_users', Object.values(onlineUsers)); // Send updated list to all users
         }
